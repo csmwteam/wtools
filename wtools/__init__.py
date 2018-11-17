57f09cf5a6862384bafdea4f784888d118873580
Bane Sullivan <banesullivan@gmail.com>
2018-11-16 17:37:26 -0700

    Bump version: 0.0.3 → 0.0.4

--- wtools/__init__.py
+++ wtools/__init__.py
@@ -5,7 +5,7 @@ from .plots import *
 from .geostats import *
 from .transform import *
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Colorado School of Mines W-Team'
